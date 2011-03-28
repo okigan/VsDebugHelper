cc236fe0ec2628afbcdf02e37f4518d585251b82
Igor Okulist <Administrator@.(none)>
2011-03-28 16:32:53 -0700

    Add and factor out commands.

---
+++ VSMemoryDumpAddin/Commands/ReadMem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnvDTE;
+using EnvDTE80;
+
+namespace VSMemoryDumpAddin.Commands {
+    class ReadMem : ICommand {
+        #region members
+        private DTE2 _application;
+        private AddIn _addin;
+        #endregion
+
+
+        #region ICommand Members
+
+        void ICommand.Initialize(EnvDTE80.DTE2 application, EnvDTE.AddIn addin) {
+            _application = application;
+            _addin = addin;
+        }
+
+
+        string ICommand.CommandText {
+            get { return "readmem"; }
+        }
+
+        void ICommand.Exec(string cmdName
+            , vsCommandExecOption executeOption
+            , ref object variantIn
+            , ref object variantOut
+            , ref bool handled
+        ) {
+            throw new NotImplementedException();
+        }
+
+        void ICommand.QueryStatus(string cmdName
+            , vsCommandStatusTextWanted neededText
+            , ref vsCommandStatus statusOption
+            , ref object commandText
+        ) {
+            switch (neededText) {
+                case vsCommandStatusTextWanted.vsCommandStatusTextWantedNone: {
+                        if (_application.Debugger.DebuggedProcesses.Count > 0) {
+                            statusOption = vsCommandStatus.vsCommandStatusSupported
+                                | vsCommandStatus.vsCommandStatusEnabled;
+                        }
+                    } break;
+            }
+
+        }
+
+        #endregion
+    }
+}
---
+++ VSMemoryDumpAddin/ICommand.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnvDTE;
+using EnvDTE80;
+
+namespace VSMemoryDumpAddin {
+    interface ICommand {
+
+        void Initialize(DTE2 application, AddIn addin);
+
+        string CommandText {
+            get;
+        }
+
+
+        void Exec(string cmdName
+            , vsCommandExecOption executeOption
+            , ref object variantIn
+            , ref object variantOut
+            , ref bool handled
+        );
+
+        void QueryStatus(string cmdName
+            , vsCommandStatusTextWanted neededText
+            , ref vsCommandStatus statusOption
+            , ref object commandText
+        );
+    }
+}

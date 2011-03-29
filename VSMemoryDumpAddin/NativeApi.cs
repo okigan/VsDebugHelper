fba9519f5a6093ed91ee8d1c920aa743dd407985
Igor Okulist <okigan@yahoo.com>
2011-03-29 16:57:20 -0700

    Att VirtualQueryEx function.

--- VSMemoryDumpAddin/NativeApi.cs
+++ VSMemoryDumpAddin/NativeApi.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices;
 
 namespace VSMemoryDumpAddin {
     class NativeApi {
+        public const uint PROCESS_QUERY_INFORMATION = (0x0400);
         public const uint PROCESS_VM_OPERATION = 0x0008;
         public const uint PROCESS_VM_READ = (0x0010);
         public const uint PROCESS_VM_WRITE = (0x0020);
@@ -38,6 +39,36 @@ namespace VSMemoryDumpAddin {
         [DllImport("kernel32.dll")]
         public static extern Int32 CloseHandle(
                 IntPtr hObject
+        );
+
+
+        [DllImport("kernel32.dll")]
+        public static extern bool VirtualProtectEx(
+                IntPtr hProcess
+            ,   IntPtr lpAddress
+            ,   UIntPtr dwSize
+            ,   uint flNewProtect
+            ,   out uint lpflOldProtect
+        );
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MEMORY_BASIC_INFORMATION {
+            public IntPtr BaseAddress;
+            public IntPtr AllocationBase;
+            public uint AllocationProtect;
+            public IntPtr RegionSize;
+            public uint State;
+            public uint Protect;
+            public uint Type;
+        }
+
+        [DllImport("kernel32.dll")]
+        public static extern bool VirtualQueryEx(
+                IntPtr hProcess
+            ,   IntPtr lpAddress
+            ,   out MEMORY_BASIC_INFORMATION lpBuffer
+            ,   uint dwLength
         );
     }
 

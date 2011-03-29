0b91909a279d591377b6b6848e26eefae6f379c6
Igor Okulist <okigan@yahoo.com>
2011-03-29 02:14:04 -0700

    Add debuggin playground project.

---
+++ TestProjectCpp/TestProjectCpp.cpp
@@ -0,0 +1,13 @@
+// TestProjectCpp.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    unsigned char* data = new unsigned char[1024];
+
+	return 0;
+}
+

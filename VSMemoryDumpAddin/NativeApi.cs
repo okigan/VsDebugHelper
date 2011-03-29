649196a14b81bb368bfb70ca3b831be1c09a06a2
Igor Okulist <okigan@yahoo.com>
2011-03-29 12:43:28 -0700

    Factor out util functions.

---
+++ VSMemoryDumpAddin/NativeApi.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace VSMemoryDumpAddin {
+    class NativeApi {
+        public const uint PROCESS_VM_READ = (0x0010);
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(
+            UInt32 dwDesiredAccess,
+            Int32 bInheritHandle,
+            UInt32 dwProcessId
+            );
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 ReadProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            [In, Out] byte[] buffer,
+            UInt32 size,
+            out IntPtr lpNumberOfBytesRead
+            );
+
+        [DllImport("kernel32.dll")]
+        public static extern Int32 CloseHandle(
+            IntPtr hObject
+            );
+    }
+
+}
---
+++ VSMemoryDumpAddin/Util.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace VSMemoryDumpAddin {
+    class Util {
+        public static Type GetExcelTypeForComObject(object excelComObject, Type type) {
+            // enum all the types defined in the interop assembly
+            System.Reflection.Assembly assembly =
+            System.Reflection.Assembly.GetAssembly(type);
+
+            return GetExcelTypeForComObject(excelComObject, assembly);
+        }
+
+        public static Type GetExcelTypeForComObject(object excelComObject, Assembly assembly) {
+            return GetExcelTypeForComObject(excelComObject, assembly.GetTypes());
+        }
+        public static Type GetExcelTypeForComObject(object excelComObject, Type[] types) {
+            // get the com object and fetch its IUnknown
+            IntPtr iunkwn = Marshal.GetIUnknownForObject(excelComObject);
+
+            // find the first implemented interop type
+            foreach (Type currType in types) {
+                // get the iid of the current type
+                Guid iid = currType.GUID;
+                if (!currType.IsInterface || iid == Guid.Empty) {
+                    // com interop type must be an interface with valid iid
+                    continue;
+                }
+
+                // query supportability of current interface on object
+                IntPtr ipointer = IntPtr.Zero;
+                Marshal.QueryInterface(iunkwn, ref iid, out ipointer);
+
+                if (ipointer != IntPtr.Zero) {
+                    // yeah, that’s the one we’re after
+                    return currType;
+                }
+            }
+
+            // no implemented type found
+            return null;
+        }
+    }
+}
